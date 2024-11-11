8bfadce4a52f993061d367782d635c8be6f5b456
Egorpuzik <egor.egorov0004@gmail.com>
2024-11-11 17:03:07 +0100

    Модифицирование кода исполняемого файла gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander';
+import { parseFile } from '../src/fileParser.js';
 
 const program = new Command();
 
@@ -8,14 +8,13 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .arguments('<filepath1> <filepath2>') 
-  .option('-f, --format [type]', 'output format') 
-  .helpOption('-h, --help', 'output usage information') 
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2, options) => {
+    const file1Data = parseFile(filepath1);
+    const file2Data = parseFile(filepath2);
+    console.log('File 1 Data:', file1Data);
+    console.log('File 2 Data:', file2Data);
+    console.log('Format:', options.format || 'default');
+  })
   .parse(process.argv);
-
-const options = program.opts();
-const [filepath1, filepath2] = program.args;
-
-console.log('Filepath1:', filepath1);
-console.log('Filepath2:', filepath2);
-console.log('Format:', options.format || 'default');
