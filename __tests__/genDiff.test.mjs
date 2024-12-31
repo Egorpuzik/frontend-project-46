5c23b39f199057a86b81c65454d0c05b556c8e4d
Egorpuzik <egor.egorov0004@gmail.com>
2024-12-31 16:20:14 +0100

    Рекурсивное сравнение

--- __tests__/genDiff.test.mjs
+++ __tests__/genDiff.test.mjs
@@ -14,66 +14,66 @@ describe('genDiff', () => {
     console.log('Content of file2.json:', fs.readFileSync(file2Path, 'utf-8'));
 
     const expectedResult = `{
-      - common: {
-          setting1: Value 1
-          setting2: 200
-          setting3: true
-          setting6: {
-              key: value
-              doge: {
-                  wow: 
-              }
-          }
-      }
-      + common: {
-          follow: false
-          setting1: Value 1
-          setting3: null
-          setting4: blah blah
-          setting5: {
-              key5: value5
-          }
-          setting6: {
-              key: value
-              ops: vops
-              doge: {
-                  wow: so much
-              }
-          }
-      }
-      - group1: {
-          baz: bas
-          foo: bar
-          nest: {
-              key: value
-          }
-      }
-      + group1: {
-          foo: bar
-          baz: bars
-          nest: str
-      }
-      - group2: {
-          abc: 12345
-          deep: {
-              id: 45
-          }
-      }
-      + group3: {
-          deep: {
-              id: {
-                  number: 45
-              }
-          }
-          fee: 100500
-      }
-    }`;
+  - common: {
+    setting1: Value 1
+    setting2: 200
+    setting3: true
+    setting6: {
+      key: value
+      doge: {
+        wow: 
+    }
+  }
+}
+  + common: {
+    follow: false
+    setting1: Value 1
+    setting3: null
+    setting4: blah blah
+    setting5: {
+      key5: value5
+  }
+    setting6: {
+      key: value
+      ops: vops
+      doge: {
+        wow: so much
+    }
+  }
+}
+  - group1: {
+    baz: bas
+    foo: bar
+    nest: {
+      key: value
+  }
+}
+  + group1: {
+    foo: bar
+    baz: bars
+    nest: str
+}
+  - group2: {
+    abc: 12345
+    deep: {
+      id: 45
+  }
+}
+  + group3: {
+    deep: {
+      id: {
+        number: 45
+    }
+  }
+    fee: 100500
+}
+}`;
     
     
 
     const diff = genDiff(file1Path, file2Path);
     console.log(diff);
-    expect(repeat).toBe(expectedResult);
+    expect(diff).toBe(expectedResult);
   });
 
   test('should return "The files are identical." for identical JSON files', () => {
