0f4de31042e322d6217071af2908d754882308aa
Egorpuzik <egor.egorov0004@gmail.com>
2025-01-25 23:19:21 +0100

    Remove unnecessary files and update project configuration

--- eslint.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  testMatch: ['**/__tests__/**/*.test.mjs'], // Укажите путь к тестовым файлам
-  extensionsToTreatAsEsm: ['.mjs'], // Расширения, которые Jest будет обрабатывать как ESM
-  transform: {
-    '^.+\\.mjs$': 'babel-jest', // Используйте babel-jest для трансформации файлов
-  },
-  transformIgnorePatterns: ['/node_modules/'], // Исключите node_modules из трансформации
-};
--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testMatch: [
-    "**/__tests__/**/*.test.mjs",  // Паттерн для поиска тестов с расширением .test.mjs в папке __tests__
-    "**/?(*.)+(spec|test).mjs"     // Паттерн для файлов с расширением .test.mjs и .spec.mjs
-  ],
-  transform: {
-    "^.+\\.(js|mjs)$": "babel-jest",  // Преобразуем .js и .mjs файлы с помощью babel-jest
-  },
-};
