a99f12e7c42a09560bcd45e98d9ae5635b7648b5
Egorpuzik <egor.egorov0004@gmail.com>
2025-02-10 10:56:38 +0100

    Update bin/gendiff.js: remove redundant code

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,10 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import path from 'path';
 import process from 'process';
 import genDiff from '../src/genDiff.js';
 
 const program = new Command();
-
-const supportedFormats = ['stylish', 'plain', 'json'];
 
 program
   .version('1.0.0')
@@ -16,8 +13,6 @@ program
   .argument('<filepath2>', 'Second file to compare')
   .option('-f, --format <type>', 'Output format', 'stylish')
   .action((filepath1, filepath2, options) => {
-    const filePath1 = path.resolve(process.cwd(), filepath1);
-    const filePath2 = path.resolve(process.cwd(), filepath2);
     
     const result = genDiff(filePath1, filePath2, options.format);
     console.log(result);
